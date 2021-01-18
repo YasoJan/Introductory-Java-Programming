22d3e563eecc7f1b8720144f84a9096eefa8c6b6
YasoJan <76984494+YasoJan@users.noreply.github.com>
2021-01-17 18:40:41 -0800

    Create starPattern.java

---
+++ Chapter_2/Programming_Challenge_4/starPattern.java
@@ -0,0 +1,27 @@
+
+/*
+Programming Challenge 4
+
+Write a program that displays the following pattern:
+    *
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+*/
+
+public class starPattern {
+
+     public static void main(String []args){
+        
+       System.out.println("   *");
+       System.out.println("  ***");
+       System.out.println(" *****");
+       System.out.println("*******");
+       System.out.println(" *****");
+       System.out.println("  ***");
+       System.out.println("   *"); 
+     }
+}
